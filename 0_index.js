40c86443d72dfa4301c3f22510ba8261f00690f4
mhkeller <code@mhkeller.com>
2023-01-13 13:20:13 -0500

    add debugger

--- 0_index.js
+++ 0_index.js
@@ -6,3 +6,8 @@ import notify from 'wsk-notify';
 const outDir = '0_out';
 
 const data = readDataSync('./0_in/');
+
+// for (let i = 0; i < Infinity; i += 1) {
+// 	await sleep(1_000);
+// 	debugger
+// }
