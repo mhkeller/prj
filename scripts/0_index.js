063985f2f0c62f14ed63c44dd4f38126601c8ec6
mhkeller <code@mhkeller.com>
2025-05-16 15:08:59 -0400

    update

--- scripts/0_index.js
+++ scripts/0_index.js
@@ -1,11 +1,11 @@
 import { join, basename } from 'path';
 import { readDataSync } from 'indian-ocean';
-import { writeSync, sleep } from '@mhkeller/utils';
+import { writeSync, sleep, initLogProgress, commas } from '@mhkeller/utils';
 import notify from '@mhkeller/notify';
 
-const outDir = '0_out';
+const outDir = 'out/0_out';
 
-const data = readDataSync('./0_in/');
+const data = readDataSync('./in/0_in/');
 
 // for (let i = 0; i < Infinity; i += 1) {
 // 	await sleep(1_000);
