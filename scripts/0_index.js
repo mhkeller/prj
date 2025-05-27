d3d176234900492ba6b8a53f706323bf51f94910
mhkeller <code@mhkeller.com>
2025-05-27 14:25:27 -0400

    add dotenv import

--- scripts/0_index.js
+++ scripts/0_index.js
@@ -2,6 +2,7 @@ import { join, basename } from 'path';
 import { readDataSync } from 'indian-ocean';
 import { writeSync, sleep, initLogProgress, commas } from '@mhkeller/utils';
 import notify from '@mhkeller/notify';
+import 'dotenv/config';
 
 const outDir = 'out/0_out';
 
